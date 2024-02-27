aed7ce111be3ddff7e0946316866bf0230119172
Anthony <amach510@gmail.com>
2024-02-27 13:05:43 -0800

    added description movie description, genre description, imageURL, and featured option

--- index.js
+++ index.js
@@ -7,103 +7,163 @@ const app = express();
     let topMovies = [
         {
             title: 'Spirited Away',
-            genre: ['Animation', 'Supernatural'],
+            description: 'Vivid and intriguing, Spirited Away tells the story of Chihiro\'s journey through an unfamiliar world as she strives to save her parents and return home.',
+            genre: {
+                    name: ['Animation', 'Supernatural'],
+                    description:'The supernatural genre encompasses storytelling that involves elements beyond the natural world, often featuring supernatural phenomena, mystical forces, paranormal activities, or entities that defy the laws of nature.'
+                    },
             director: {
                     directorName: 'Miyazaki, Hayao',
                     bio: 'Founder of Studio Ghibli',
                     birth: '1941'
                 },
-            date: '2001'
+            imageURL: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTqTEoJUOlTg4HakvM3SHU0a7a3gFpAQ1HrBL21fBAr1OYOeocP",
+            date: '2001',
+            featured: false
         },
         {
             title: 'Your Name',
-            genre: ['Animation', 'Drama', 'Supernatural'],
+            description: 'Your Name revolves around Mitsuha and Taki\'s actions, which begin to have a dramatic impact on each other\'s lives, weaving them into a fabric held together by fate and circumstance.',
+            genre: {
+                    name: ['Animation', 'Drama', 'Supernatural'],
+                    description: 'The drama genre typically explores realistic and emotionally charged narratives, focusing on the complexities of human relationships, personal growth, and the everyday struggles and triumphs of characters in various life situations.'
+                },
             director: {
                     directorName: 'Shinkai, Makoto',
                     bio: 'Regarded as "The New Miyazki"',
                     birth: '1973'
                 },
-            date: '2016'
+            imageURL: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSdCn7P_niNCbNFHt9vLgDc-YlRIhwvnCPtHzyiHVP_GK-XmDS1",
+            date: '2016',
+            featured: false
         },
         {
             title: 'Weathering with You',
-            genre: ['Animation', 'Drama', 'Fantasy', 'Romance'],
+            description: 'Enchanting yet bittersweet love story between a teenage boy who can control the weather and a girl with a mysterious ability, set against the backdrop of a Tokyo plagued by incessant rain.',
+            genre: {
+                    name: ['Animation','Romance', 'Drama', 'Fantasy'],
+                    description: 'Romance is a genre characterized by narratives that center around love and emotional relationships, often highlighting the development of romantic connections, challenges, and the pursuit of intimacy between characters.'
+                },
             director: {
                     directorName: 'Shinkai, Makoto',
                     bio: 'Regarded as "The New Miyazki"',
                     birth: '1973'
                 },
-            date: '2019'
+            imageURL: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQfiDnbbGZmmZfAnbjsU_AKKAsRfa4VmskkCWbhrmrYqnVtZWq2",
+            date: '2019',
+            featured: false
         },
         {
             title: 'My Neighbor Totoro',
-            genre: ['Animation', 'Supernatural'],
+            description: 'The magical adventures of two young sisters who befriend forest spirits, particularly the lovable creature Totoro, while navigating the challenges of moving to a new countryside home.',
+            genre: {
+                name: ['Animation', 'Supernatural'],
+                description:'The supernatural genre encompasses storytelling that involves elements beyond the natural world, often featuring supernatural phenomena, mystical forces, paranormal activities, or entities that defy the laws of nature.'
+                },
             director: {
                     directorName: 'Miyazaki, Hayao',
                     bio: 'Founder of Studio Ghibli',
                     birth: '1941'
                 },
-            date: '1988'
+            imageURL: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcShhQmWrd7gqouln_mhRDdoLy8MkIOwZj1cNx7xhUU2I4cB0IEJ',
+            date: '1988',
+            featured: false
         },
         {
-            title: 'Nausicaa of the Valley of the Wind',
-            genre: ['Animation', 'Adventure', 'Fantasy'],
+            title: 'Nausicaä of the Valley of the Wind',
+            description: 'The courageous Princess Nausicaä strives to bring harmony between humanity and the environment in a post-apocalyptic world threatened by toxic jungles and warring nations.',
+            genre:{ 
+                    name: ['Animation', 'Adventure', 'Fantasy'],
+                    description: 'The adventure genre encompasses narratives that revolve around characters undertaking perilous journeys, quests, or explorations, often featuring excitement, challenges, and a sense of discovery in unfamiliar and thrilling environments.'
+                },
             director: {
                     directorName: 'Miyazaki, Hayao',
                     bio: 'Founder of Studio Ghibli',
                     birth: '1941'
                 },
-            date: '1984'
+            imageURL: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRGi4HGfC4oJM34jPoaTDWLvMOlNxiXk0DZSyO57mQtZb2FirdB',
+            date: '1984',
+            featured: false
         },
         {
             title: 'Redline',
-            genre: ['Animation', 'Action', 'Sci-Fi'],
+            description: 'High-octane Japanese animated film, featuring intense, intergalactic racing competition as daredevil racer JP strives to win the ultimate Redline race on a dangerous and visually stunning alien planet.',
+            genre:{ 
+                    name: ['Animation', 'Sci-Fi', 'Action'],
+                    description: 'Science fiction, or sci-fi, is a genre that explores speculative and futuristic concepts, often incorporating advanced technology, space exploration, and scientific principles to envision alternative realities, possible futures, and the impact of scientific advancements on society.'
+                },
             director: {
                     directorName: 'Koike, Takeshi',
                     bio: 'Protegee of Yoshiaki Kawajiri',
                     birth: '1968'
                 },
-            date: '2009'
+            imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-r0UhA_dp_pNbQQY1l1GytMTOEAS6qD-413w9f-2ME5tg6GEF',
+            date: '2009',
+            featured: false
         },
         {
             title: 'Princess Mononoke',
-            genre: ['Animation', 'Action', 'Adventure', 'Fantasy'],
+            description: 'Chronicling the journey of a young warrior named Ashitaka, who becomes entangled in a conflict between industrial humans and nature spirits, led by the fierce and enigmatic Princess Mononoke, in a beautifully crafted tale of environmentalism and the struggle for balance.',
+            genre:{
+                    name: ['Animation', 'Fantasy', 'Action', 'Adventure'],
+                    description: 'Fantasy is a genre characterized by imaginative and often magical elements, where stories unfold in fantastical worlds with mythical creatures, magical powers, and extraordinary settings that go beyond the bounds of reality.'
+                },
             director: {
                     directorName: 'Miyazaki, Hayao',
                     bio: 'Founder of Studio Ghibli',
                     birth: '1941'
                 },
-            date: '1997'
+            imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQntZTJb1c6O4iFaT_Ll3w4a-R3TzlpID2dJ0wDdwjemnMbOx7r',
+            date: '1997',
+            featured: false
         },
         {
             title: 'Kiki\'s Delivery Service',
-            genre: ['Animation', 'Adventure', 'Comedy', 'Drama', 'Fantasy'],
+            description: 'Following the coming-of-age journey of a young witch named Kiki as she establishes her own delivery service in a quaint seaside town, discovering friendship and self-confidence along the way.',
+            genre: {
+                    name: ['Animation', 'Adventure', 'Comedy', 'Drama', 'Fantasy'],
+                    description: 'The adventure genre encompasses narratives that revolve around characters undertaking perilous journeys, quests, or explorations, often featuring excitement, challenges, and a sense of discovery in unfamiliar and thrilling environments.'
+                },
             director: {
                     directorName: 'Miyazaki, Hayao',
                     bio: 'Founder of Studio Ghibli',
                     birth: '1941'
                 },
-            date: '1989'
+            imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHAXQmo6UAT_P8xIAppBglmEruxxExDBGELnwXCqjDjZToWwXr',
+            date: '1989',
+            featured: false
         },
         {
             title: 'A Silent Voice',
-            genre: ['Animation', 'Drama'],
+            description: 'Delving into themes of redemption and empathy as it tells the story of a former bully seeking redemption and connection with a deaf classmate he once tormented in elementary school.',
+            genre: {
+                name: ['Animation', 'Drama'],
+                description: 'The drama genre typically explores realistic and emotionally charged narratives, focusing on the complexities of human relationships, personal growth, and the everyday struggles and triumphs of characters in various life situations.'
+            },
             director: {
                     directorName: 'Yamada, Naoko',
                     bio: 'Directed the K-On! and Tamako Market anime series',
                     birth: '1984'
                 },
-            date: '2016'
+            imageURL: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQJrlYXvqlm1bAFMMfjhlX970K4B0z2AJv66KKb1Y9gW-4eml2X',
+            date: '2016',
+            featured: false
         },
         {
             title: 'Ride Your Wave',
-            genre: ['Animation', 'Drama', 'Romance', 'Supernatural'],
+            description: 'Blending romance and fantasy as it follows a young woman who discovers the transformative power of love and resilience after her surfer boyfriend\'s tragic death.',
+            genre: {
+                    name: ['Animation', 'Drama', 'Romance', 'Supernatural'],
+                    description: 'The drama genre typically explores realistic and emotionally charged narratives, focusing on the complexities of human relationships, personal growth, and the everyday struggles and triumphs of characters in various life situations.'
+                },
             director: {
                     directorName: 'Yuasa, Masaaki',
                     bio:'Co-founded Science SARU',
                     birth:'1965'
                 },
-            date: '2020'
+            imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5FoZxavLyV9NpxWm1XGPwVEQ0qAon_B6KIv55nJb3BAjfAX-V',
+            date: '2020',
+            featured: false
         }
     ];
 
