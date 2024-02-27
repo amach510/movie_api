1525c3d20d918d17008f1f15bc1c6cc2c5317a0e
Anthony <amach510@gmail.com>
2024-02-26 18:39:03 -0800

    added genres and director details

--- index.js
+++ index.js
@@ -7,42 +7,102 @@ const app = express();
     let topMovies = [
         {
             title: 'Spirited Away',
+            genre: ['Animation', 'Supernatural'],
+            director: {
+                    directorName: 'Miyazaki, Hayao',
+                    bio: 'Founder of Studio Ghibli',
+                    birth: '1941'
+                },
             date: '2001'
         },
         {
             title: 'Your Name',
+            genre: ['Animation', 'Drama', 'Supernatural'],
+            director: {
+                    directorName: 'Shinkai, Makoto',
+                    bio: 'Regarded as "The New Miyazki"',
+                    birth: '1973'
+                },
             date: '2016'
         },
         {
-            title: 'Wandering with You',
+            title: 'Weathering with You',
+            genre: ['Animation', 'Drama', 'Fantasy', 'Romance'],
+            director: {
+                    directorName: 'Shinkai, Makoto',
+                    bio: 'Regarded as "The New Miyazki"',
+                    birth: '1973'
+                },
             date: '2019'
         },
         {
             title: 'My Neighbor Totoro',
+            genre: ['Animation', 'Supernatural'],
+            director: {
+                    directorName: 'Miyazaki, Hayao',
+                    bio: 'Founder of Studio Ghibli',
+                    birth: '1941'
+                },
             date: '1988'
         },
         {
             title: 'Nausicaa of the Valley of the Wind',
+            genre: ['Animation', 'Adventure', 'Fantasy'],
+            director: {
+                    directorName: 'Miyazaki, Hayao',
+                    bio: 'Founder of Studio Ghibli',
+                    birth: '1941'
+                },
             date: '1984'
         },
         {
             title: 'Redline',
+            genre: ['Animation', 'Action', 'Sci-Fi'],
+            director: {
+                    directorName: 'Koike, Takeshi',
+                    bio: 'Protegee of Yoshiaki Kawajiri',
+                    birth: '1968'
+                },
             date: '2009'
         },
         {
             title: 'Princess Mononoke',
+            genre: ['Animation', 'Action', 'Adventure', 'Fantasy'],
+            director: {
+                    directorName: 'Miyazaki, Hayao',
+                    bio: 'Founder of Studio Ghibli',
+                    birth: '1941'
+                },
             date: '1997'
         },
         {
             title: 'Kiki\'s Delivery Service',
+            genre: ['Animation', 'Adventure', 'Comedy', 'Drama', 'Fantasy'],
+            director: {
+                    directorName: 'Miyazaki, Hayao',
+                    bio: 'Founder of Studio Ghibli',
+                    birth: '1941'
+                },
             date: '1989'
         },
         {
             title: 'A Silent Voice',
+            genre: ['Animation', 'Drama'],
+            director: {
+                    directorName: 'Yamada, Naoko',
+                    bio: 'Directed the K-On! and Tamako Market anime series',
+                    birth: '1984'
+                },
             date: '2016'
         },
         {
             title: 'Ride Your Wave',
+            genre: ['Animation', 'Drama', 'Romance', 'Supernatural'],
+            director: {
+                    directorName: 'Yuasa, Masaaki',
+                    bio:'Co-founded Science SARU',
+                    birth:'1965'
+                },
             date: '2020'
         }
     ];
