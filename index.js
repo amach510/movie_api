bd4f17047a653414f7373bcbb9ae7aea26b71b75
Anthony <amach510@gmail.com>
2024-05-06 17:39:06 -0700

    uncommented

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'https://m
 app.use(cors({
 origin: (origin, callback) => {
     if(!origin) return callback(null, true);
-    // if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-    // let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
-    // return callback(new Error(message ), false);
-    // }
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+    let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+    return callback(new Error(message ), false);
+    }
     return callback(null, true);
 }
 }));
@@ -59,9 +59,9 @@ app.post('/users', [
     // check the validation object for errors
     let errors = validationResult(req);
 
-    // if (!errors.isEmpty()) {
-    // return res.status(422).json({ errors: errors.array() });
-    // }
+    if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+    }
 
     let hashedPassword = Users.hashPassword(req.body.password);
     await Users.findOne({ username: req.body.Username })
